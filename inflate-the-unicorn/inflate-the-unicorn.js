77b77ad90d9f2e44608d4dce4e47e9a6f53473b6
Louis Nguyen <103094279+louis-nguyen108@users.noreply.github.com>
2022-05-11 20:56:36 +1200

    Add logic to cycle images, and logic to alert when last image is reached

--- inflate-the-unicorn/inflate-the-unicorn.js
+++ inflate-the-unicorn/inflate-the-unicorn.js
@@ -4,9 +4,7 @@
 
 console.log('Inflate The Unicorn!')
 
-// need an object
-
-let unicorns = document.getElementsByClassName('inflate-an-image')
+let unicorns = document.getElementsByClassName('unicorns')
 // console.log(unicorns)
 
 // bind onclick to all img tag using for loop
@@ -14,13 +12,20 @@ for (i = 0; i < unicorns.length; i++) {
   unicorns[i].onclick = inflate
 }
 
-// // function to inflate the unicorn
-
+// function to inflate the balloon
 function inflate(e) {
-  // cycle to the next image when an image is clicked
-  // logic to cycle through images => using loop?
-  // logic to keep track of which image has been clicked and what to cycle to next using loop ??
-  // logic to stop once reached last image (balloon fully inflated => unicorn-3.png) => using if statement ??
-  let unicorn = e.target
-  // console.log(unicorn)
+  // logic to cycle to the next image
+  // logic to alert when last image is reached (balloon fully inflated)
+  let newImg = e.target
+  // console.log(newImg.src)
+  if (newImg.src.match('./images/unicorn-0.png')) {
+    newImg.src = './images/unicorn-1.png'
+  } else if (newImg.src.match('./images/unicorn-1.png')) {
+    newImg.src = './images/unicorn-2.png'
+  } else if (newImg.src.match('./images/unicorn-2.png')) {
+    newImg.src = './images/unicorn-3.png'
+  } else if (newImg.src.match('./images/unicorn-3.png')) {
+    console.log('Balloon fully inflated')
+    alert('Unicorn number ' + e.target.id + ' says thank you')
+  }
 }
