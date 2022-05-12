0861931c24f1c240e73ab69edd06468fd4457e73
Louis Nguyen <103094279+louis-nguyen108@users.noreply.github.com>
2022-05-12 17:52:10 +1200

    Add function newMole() to randomly display image in different table cell

--- whack-a-mole/whack-a-mole.js
+++ whack-a-mole/whack-a-mole.js
@@ -10,45 +10,59 @@ console.log('Whack-a-Mole!')
 // A way to show a mole in the chosen cell.
 
 // --- A WAY TO REFERENCE THE CELL
-let cells = document.getElementsByTagName('td')
-// console.log(cells)
+let moleSpots = document.getElementsByTagName('td')
+let spotsArr = Array.from(moleSpots)
+// console.log(spotsArr)
 
 // code to bind onclick
-
-// --- RANDOM FUNCTION/LOGIC
-// --- reference min = 0
-// --- reference max = 24
-function randomIndexNum(min, max) {
-  min = Math.ceil(min)
-  max = Math.floor(max)
-
-  do {
-    randomNum = Math.floor(Math.random() * (max - min + 1) + min)
-  } while (randomNum === randomIndexNum.last) // logic to not display the same number twice in a row
-  randomIndexNum.last = randomNum
-  return randomNum
-}
+// for (i = 0; i < spotsArr.length; i++) {
+//   spotsArr[i].onclick = moleWhacked
+// }
 
 // FUNCTION TO CALL WHEN CLICKED
 function moleWhacked(e) {
   // code to target the click
   // if there is mole,
-  // then remove mole (image) from target cell
-  // then randomly select a cell through random logic
-  // then display mole (image) on the randomly selected cell
+  /* then remove mole (image) from target cell
+     then randomly select a cell through random logic
+     then display mole (image) on the randomly selected cell */
   // if there is no mole (empty cell)
-  // then nothing happens until a cell with mole has been clicked
-
-  let cell = e.target
+  /* then nothing happens until a cell with mole has been clicked */
+  // console.log('WHACKED!')
+  // let spot = e.target
+  // const moleImgTag = document.createElement('img')
+  // const spot = document.createElement('td')
+  // spot.appendChild(moleImgTag)
 }
 
-//
-//
+// --- SELECT CELL AND ATTACH IMG
+function newMole() {
+  let moleImgTag = document.createElement('img')
+  moleImgTag.classList.add = 'mole'
+  moleImgTag.src = 'mole.PNG'
+  moleImgTag.style = 'height: 200px; width: 200px;'
+  newSpot = spotsArr[getRandomNum(0, 24)]
+  newSpot.appendChild(moleImgTag)
+}
 
-// relative path to mole image
-// /whack-a-mole/mole.PNG
+// --- RANDOM FUNCTION/LOGIC
+// --- reference min = 0
+// --- reference max = 24
+function getRandomNum(min, max) {
+  min = Math.ceil(min)
+  max = Math.floor(max)
 
-//  ----- STRETCH -----
+  do {
+    randomNum = Math.floor(Math.random() * (max - min + 1) + min)
+  } while (randomNum === getRandomNum.last)
+  getRandomNum.last = randomNum // logic to NOT display the same number twice in a row
+  return randomNum
+}
+
+// --- REFERENCE mole image  tag
+// <img src="mole.png" style="height: 200px auto; width: 200px auto"></img>
+
+//  ----- STRETCH IDEAS FOR LATER -----
 
 // Add a start button to start game which once clicked, timer will start
 
